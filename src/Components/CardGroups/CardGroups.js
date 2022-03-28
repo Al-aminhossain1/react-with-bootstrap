061183120533d1b800cf84a4fe487d77c9684190
SR-Shahen <s.r.shahenda@gamil.com>
2022-03-28 18:58:04 +0600

    first commit

---
+++ src/Components/CardGroups/CardGroups.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardGroups = () => {
+    return (
+        <div>
+            <h1>This is my Card</h1>
+        </div>
+    );
+};
+
+export default CardGroups;
